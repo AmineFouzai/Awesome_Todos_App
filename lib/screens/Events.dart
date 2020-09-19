21bd6e62b083484e33d8ec94d5e4a232785d1bec
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-09-19 23:34:46 +0100

    Update Events.dart

--- lib/screens/Events.dart
+++ lib/screens/Events.dart
@@ -12,14 +12,164 @@ class Events extends StatefulWidget {
 }
 
 class _EventsState extends State<Events> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  List<String> eventsItem = events;
 
+  var Time = new DateTime.now();
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  TextEditingController _textFieldController = TextEditingController();
   _searchItem() {
-    return null;
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Search'),
+            content: TextField(
+              controller: _textFieldController,
+              decoration: InputDecoration(hintText: "Events in List"),
+            ),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              new FlatButton(
+                child: new Text('ok'),
+                onPressed: () {
+                  setState(() {
+                    //_textFieldController.text
+                    if (eventsItem.indexOf(_textFieldController.text) != -1) {
+                      String result = eventsItem[
+                          eventsItem.indexOf(_textFieldController.text)];
+                      eventsItem = [];
+                      eventsItem.add(result);
+                      Navigator.of(context).pop();
+                    } else {
+                      Navigator.of(context).pop();
+                    }
+                  });
+                },
+              )
+            ],
+          );
+        });
   }
 
   _addEventItem() {
-    return null;
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Events'),
+            content: TextField(
+              controller: _textFieldController,
+              decoration: InputDecoration(hintText: "Events in Dialog"),
+            ),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              new FlatButton(
+                child: new Text('ADD'),
+                onPressed: () {
+                  setState(() {
+                    eventsItem.add(_textFieldController.text);
+                  });
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  Widget _buildEventItem(BuildContext context) {
+    return new ListView.builder(
+      itemBuilder: (key, index) {
+        if (index < eventsItem.length) {
+          return Dismissible(
+            key: Key('item ${eventsItem[index]}'),
+            background: Container(
+              color: Colors.red,
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(Icons.delete, color: Colors.white),
+                    Text('Move to trash',
+                        style: TextStyle(color: Colors.white)),
+                  ],
+                ),
+              ),
+            ),
+            secondaryBackground: Container(
+              color: Colors.red,
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Icon(Icons.delete, color: Colors.white),
+                    Text('Move to trash',
+                        style: TextStyle(color: Colors.white)),
+                  ],
+                ),
+              ),
+            ),
+            confirmDismiss: (DismissDirection direction) async {
+              return await showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text("Delete Confirmation"),
+                    content: const Text(
+                        "Are you sure you want to delete this item?"),
+                    actions: <Widget>[
+                      FlatButton(
+                          onPressed: () {
+                            Navigator.of(context).pop(true);
+                          },
+                          child: const Text("Delete")),
+                      FlatButton(
+                        onPressed: () {
+                          Navigator.of(context).pop(false);
+                        },
+                        child: const Text("Cancel"),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            onDismissed: (DismissDirection direction) {
+              if (direction == DismissDirection.startToEnd) {
+                setState(() {
+                  eventsItem.removeAt(index);
+                });
+              } else {
+                setState(() {
+                  eventsItem.removeAt(index);
+                });
+              }
+            },
+            child: ListTile(
+              leading: Icon(
+                Icons.local_activity,
+                color: Colors.purpleAccent,
+                size: 50,
+              ),
+              title: Text(eventsItem[index]),
+              subtitle: Text(Time.toString()),
+            ),
+          );
+        }
+      },
+    );
   }
 
   @override
@@ -46,7 +196,7 @@ class _EventsState extends State<Events> {
           },
         ),
         backgroundColor: Colors.purpleAccent,
-        title: Text("Todo App"),
+        title: Text("Events"),
         centerTitle: true,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.vertical(
@@ -63,6 +213,7 @@ class _EventsState extends State<Events> {
           )
         ],
       ),
+      body: _buildEventItem(context),
       floatingActionButton: FabCircularMenu(
         fabOpenIcon: Icon(
           Icons.menu,
