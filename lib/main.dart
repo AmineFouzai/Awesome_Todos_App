60a4f722a7af49be4f8aabbf191fdc5b68ef6118
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-09-22 01:25:01 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/screens/Home.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'package:todo_app/screens/User.dart';
+
+Stopwatch excTime = new Stopwatch()..start();
 
 void main() {
   runApp(MyApp());
@@ -9,13 +12,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Home(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: SplashScreen(
+          title: Text(
+            'Welcome ,To Awsome Todos',
+            style: TextStyle(
+                color: Colors.purpleAccent,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                fontSize: 20.0),
+          ),
+          image: Image.asset("images/push-pin.png"),
+          seconds: 3,
+          loaderColor: Colors.purpleAccent,
+          navigateAfterSeconds: User(),
+        ));
   }
 }
