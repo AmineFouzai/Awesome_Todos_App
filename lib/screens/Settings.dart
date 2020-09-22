b12429e73b953347e01cdc041873d5731da9e6db
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-09-22 01:21:51 +0100

    Add files via upload

--- lib/screens/Settings.dart
+++ lib/screens/Settings.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/screens/Actions.dart';
+import 'package:todo_app/screens/Home.dart';
+import 'package:todo_app/screens/Menu.dart';
 
 class Settings extends StatefulWidget {
   Settings({Key key}) : super(key: key);
@@ -8,10 +11,47 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  GlobalKey<ScaffoldState> _scaffoldkey = new GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Settings section"),
+      child: Scaffold(
+          key: _scaffoldkey,
+          drawer: Container(
+            margin: EdgeInsets.all(0),
+            padding: EdgeInsets.all(0),
+            child: Menu(),
+          ),
+          appBar: AppBar(
+            automaticallyImplyLeading: true,
+            title: Text("Settings"),
+            backgroundColor: Colors.purpleAccent,
+            centerTitle: true,
+            shape: RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.vertical(bottom: Radius.circular(30))),
+            leading: Padding(
+              padding: const EdgeInsets.fromLTRB(30, 0, 0, 8),
+              child: IconButton(
+                  icon: Icon(
+                    Icons.menu,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    _scaffoldkey.currentState.openDrawer();
+                  }),
+            ),
+          ),
+          body: SettingsActions(),
+          floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.purpleAccent,
+              child: Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Home()),
+                );
+              })),
     );
   }
 }
